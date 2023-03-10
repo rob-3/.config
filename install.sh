367e8f9348c750418e01431de4ec977b3945791c
Robert Boyd III <git@robertboyd.dev>
2023-03-09 23:30:58 -0500

    Add more modifications to dotfiles

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 
 # Install neovim
 original_dir=$(pwd)
-cd /tmp
+cd ~
 curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage > nvim.appimage
 chmod u+x ./nvim.appimage
 ./nvim.appimage --appimage-extract
-alias nvim="/tmp/squashfs-root/AppRun"
 cd $original_dir
+
+cp nvimwrapper /usr/bin/nvim
 
 mkdir ~/.config
 cp -r * ~/.config/
