c65c615f21125185f4345ab41d687ecbca98df57
Robert Boyd III <git@robertboyd.dev>
2023-03-09 23:01:22 -0500

    Test codespaces setup

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# Install neovim
+original_dir=$(pwd)
+cd /tmp
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage > nvim.appimage
+chmod u+x ./nvim.appimage
+./nvim.appimage --appimage-extract
+alias nvim="/tmp/squashfs-root/AppRun"
+cd $original_dir
+
 cp -r * ~/.config/
